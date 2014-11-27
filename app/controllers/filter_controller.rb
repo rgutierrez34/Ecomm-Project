b90377ba0e447f4ca1daccccf65795a38acaf39a
Ronald Gutierrez <rongutierrez@gmail.com>
2014-11-26 19:02:59 -0600

    Added implementation of search by category

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -9,6 +9,14 @@ class FilterController < ApplicationController
 
   def search
   	wildcard_keywords = '%' + params[:search_keywords] + '%'
-  	@products = Product.all.where("name LIKE ?", wildcard_keywords)
+  	# @products = Product.all.where("name LIKE ?", wildcard_keywords)
+
+    if params[:category_id]
+      category = Category.find(params[:category_id])
+      @products = category.products.where("name LIKE ?", wildcard_keywords) 
+    else
+      @products = Product.where("name LIKE ?", wildcard_keywords)
+    end
+
   end
 end
