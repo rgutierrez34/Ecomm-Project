74c7edbaff19c58ada5440ec966e6df65ad74a9b
Ronald Gutierrez <rongutierrez@gmail.com>
2014-12-09 16:06:10 -0600

    Installed rubocop gem and created config yml

--- app/controllers/contact_about_controller.rb
+++ app/controllers/contact_about_controller.rb
@@ -1,9 +1,9 @@
 class ContactAboutController < ApplicationController
-	def contact
-		@contactabouts = ContactAbout.first
-	end
+  def contact
+    @contactabouts = ContactAbout.first
+  end
 
-	def about
-		@contactabouts = ContactAbout.first
-	end
+  def about
+    @contactabouts = ContactAbout.first
+  end
 end
--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -1,15 +1,15 @@
 class FilterController < ApplicationController
   def sale
-  	@products = Product.where(sale: true).page(params[:page]).per(8)
+    @products = Product.where(sale: true).page(params[:page]).per(8)
   end
 
   def new 
-  	@products = Product.limit(5).reverse_order
+    @products = Product.limit(5).reverse_order
   end
 
   def search
-  	wildcard_keywords = '%' + params[:search_keywords] + '%'
-  	# @products = Product.all.where("name LIKE ?", wildcard_keywords)
+    wildcard_keywords = '%' + params[:search_keywords] + '%'
+    # @products = Product.all.where("name LIKE ?", wildcard_keywords)
 
     if params[:category_id]
       category = Category.find(params[:category_id])
@@ -17,6 +17,5 @@ class FilterController < ApplicationController
     else
       @products = Product.where("name LIKE ?", wildcard_keywords)
     end
-
   end
 end
